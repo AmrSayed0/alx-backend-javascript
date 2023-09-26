de7f118bc29907e9210aaace70788c5a825c24b1
AmrSayed0 <amracademycode@gmail.com>
2023-09-26 19:24:38 +0300

    Add getResponseFromAPI function

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,6 @@
+// This function returns a resolved Promise with the value 'Resolved'.
 export default function getResponseFromAPI() {
   return new Promise((resolve) => {
-    resolve('Resolved');
+    resolve("Resolved");
   });
 }
