7c027c782c10a5006170c77d1c0bbb04613839f8
AmrSayed0 <amracademycode@gmail.com>
2023-09-26 18:50:50 +0300

    Add handledResponseFromAPI function for handling

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,21 +1,11 @@
-/**
- * Handles the response from an API Promise, providing a default success response
- * and logging that a response was received.
- *
- * @param {Promise<any>} promise - The Promise to handle the response for.
- * @returns {Promise<Object>} A Promise that resolves with a default response object
- *                            and logs a message when the Promise settles.
- */
 export default function handledResponseFromAPI(promise) {
   // Create a default success response object.
   const res = { status: 200, body: "success" };
-
   // Use Promise chaining to handle the response.
   return promise
-    .then(() => res) // Resolve with the default response on success.
-    .catch(() => new Error()) // Catch any errors and create a generic error object.
+    .then(() => res)
+    .catch(() => new Error())
     .finally(() => {
-      // Log a message when the Promise settles (regardless of success or failure).
       console.log("Got a response from the API");
     });
 }
