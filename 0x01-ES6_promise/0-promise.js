2fc82dd7793224b6680f865a817f75d317445600
Amr Sayed <amracademycode@gmail.com>
2023-09-26 19:26:32 +0300

    Delete 0x01-ES6_promise directory

--- 0x01-ES6_promise/0-promise.js
+++
@@ -1,6 +0,0 @@
-// This function returns a resolved Promise with the value 'Resolved'.
-export default function getResponseFromAPI() {
-  return new Promise((resolve) => {
-    resolve("Resolved");
-  });
-}
