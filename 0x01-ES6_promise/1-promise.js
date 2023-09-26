3dabcb07fe0db1924a4884abbb8dce01d5677a69
AmrSayed0 <amracademycode@gmail.com>
2023-09-26 14:23:42 +0300

    Fix issue: Add getFullResponseFromAPI function

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,18 +1,13 @@
-/**
- * Returns a Promise that simulates a response from a fake API.
- *
- * @param {boolean} success - A boolean indicating whether the API call should succeed.
- * @returns {Promise<Object>} A Promise that resolves with a response object if successful,
- *                            or rejects with an error if unsuccessful.
- */
-export default function getFullResponseFromAPI(success) {
+function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
-      // If 'success' is true, simulate a successful API response.
+      // Resolve with a successful response object
       resolve({ status: 200, body: "Success" });
     } else {
-      // If 'success' is false, simulate a failed API response with an error
+      // Reject with an error message
       reject(new Error("The fake API is not working currently"));
     }
   });
 }
+
+export default getFullResponseFromAPI;
