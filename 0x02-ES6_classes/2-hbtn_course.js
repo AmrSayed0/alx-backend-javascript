90b2c36ae00e4239282ac62aa1dd474f600a31f3
AmrSayed0 <amracademycode@gmail.com>
2023-09-29 00:22:15 +0300

    Updated: Implement HolbertonCourse class with getters and setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,46 +1,56 @@
-export default class HolbertonCourse {
+class HolbertonCourse {
   constructor(name, length, students) {
     this._name = name;
     this._length = length;
-    this._students = Array.isArray(students) ? students : [];
+    this._students = students;
+
+    if (typeof name !== "string") {
+      throw new TypeError("Name must be a string");
+    }
+
+    if (typeof length !== "number") {
+      throw new TypeError("Length must be a number");
+    }
   }
 
-  // Getter and Setter for 'name'
+  // Getter for name
   get name() {
     return this._name;
   }
 
+  // Setter for name
   set name(newName) {
-    if (typeof newName === "string") {
-      this._name = newName;
-    } else {
-      throw new Error("Invalid data type. Name must be a string.");
+    if (typeof newName !== "string") {
+      throw new TypeError("Name must be a string");
     }
+    this._name = newName;
   }
 
-  // Getter and Setter for 'length'
+  // Getter for length
   get length() {
     return this._length;
   }
 
+  // Setter for length
   set length(newLength) {
-    if (typeof newLength === "number") {
-      this._length = newLength;
-    } else {
-      throw new Error("Invalid data type. Length must be a number.");
+    if (typeof newLength !== "number") {
+      throw new TypeError("Length must be a number");
     }
+    this._length = newLength;
   }
 
-  // Getter and Setter for 'students'
+  // Getter for students
   get students() {
     return this._students;
   }
 
+  // Setter for students
   set students(newStudents) {
-    if (Array.isArray(newStudents)) {
-      this._students = newStudents;
-    } else {
-      throw new Error("Invalid data type. Students must be an array.");
+    if (!Array.isArray(newStudents)) {
+      throw new TypeError("Students must be an array");
     }
+    this._students = newStudents;
   }
 }
+
+export default HolbertonCourse;
