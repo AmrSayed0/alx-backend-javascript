7f4c218693f2800a67e6fb4dac32d52ce3e1b78b
AmrSayed0 <amracademycode@gmail.com>
2023-10-03 14:14:30 +0300

    Fix issues: Create a a Basic list of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,19 +1,13 @@
-export default function getListStudents() {
-  return [
-    {
-      id: 1,
-      firstName: "Guillaume",
-      location: "San Francisco",
-    },
-    {
-      id: 2,
-      firstName: "James",
-      location: "Columbia",
-    },
-    {
-      id: 5,
-      firstName: "Serena",
-      location: "San Francisco",
-    },
+// 0-get_list_students.js
+
+function getListStudents() {
+  const students = [
+    { id: 1, firstName: "Guillaume", location: "San Francisco" },
+    { id: 2, firstName: "James", location: "Columbia" },
+    { id: 5, firstName: "Serena", location: "San Francisco" },
   ];
+
+  return students;
 }
+
+export default getListStudents;
